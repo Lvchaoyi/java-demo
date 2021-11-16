121ceab75e9990f6eca2e932ebf221aaaefa1565
Lvchaoyi <lovelysmart250@gmail.com>
2021-11-17 00:34:27 +0800

    单例模式

---
+++ src/designpattern/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package designpattern.singleton;
+
+public class Singleton {
+//    volatile 主要是为了防止另一个线程在getInstance时读取到上一个线程创建实例时由于指令重排序导致的instance未初始化（先引用指向空间，再初始化对象）的情况
+    private volatile static Singleton instance;
+    private Singleton() {}
+    public Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
